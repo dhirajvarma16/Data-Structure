0f8cd0fb5186364a6ee78434b48890eed321cb3a
Dhiraj varma <49423650+dhirajvarma16@users.noreply.github.com>
2020-04-20 16:00:35 +0530

    Add files via upload
    Data Structure Java

---
+++ JavaProgramPractice/square_root_bineary_tree.java
@@ -0,0 +1,26 @@
+package JavaProgramPractice;
+
+public class square_root_bineary_tree {
+
+	public static int binery_tree() {
+		int arr = 9, mid_value = 0;
+		int start_value = 1, end_value = arr;
+
+		while (start_value <= end_value) {
+
+			if (mid_value * mid_value == arr) {
+				return mid_value;
+			} else if (mid_value * mid_value > arr) {
+				end_value = end_value - 1;
+			} else {
+				start_value = start_value + 1;
+			}
+
+		}
+		return end_value;
+	}
+
+	public static void main(String[] args) {
+		System.out.print("" + binery_tree());
+	}
+}
